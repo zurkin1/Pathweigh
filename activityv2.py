7dd3f8a9cad3355a8e11958785b6b0c7fc44fdac
Dani Livne <33825810+zurkin1@users.noreply.github.com>
2022-05-13 18:35:10 +0300

    Change.

--- activityv2.py
+++ activityv2.py
@@ -8,13 +8,13 @@ def process_samples(udp_chunk):
     mapping = kegg['mapping']
     path_inter_ids = kegg['pathList'][path_id]
     for inter_id in path_inter_ids:
+        total_activity = 0
+        total_inter = 0
         interaction = kegg['interactionList'].loc[kegg['interactionList']['interaction.id'] == inter_id]
         inputm = interaction.iloc[0]['input']
         outputm = interaction.iloc[0]['output']
         node_in = mapping.loc[mapping['node.id'] == inputm]
         node_out = mapping.loc[mapping['node.id'] == outputm]
-        total_activity = 0
-        total_inter = 0
         try:
           activity_in = udp[sample].loc[[str.lower(x) for x in node_in['symbol'].values]].prod()
           activity_out = udp[sample].loc[[str.lower(x) for x in node_out['symbol'].values]].prod()
@@ -29,16 +29,16 @@ def process_samples(udp_chunk):
     #nt[ind]
 
 
-#udp = pd.read_csv('Pathweigh/data/output_udp_zurkin.csv', index_col = 0)
+udp = pd.read_csv('./data/output_udp.csv', index_col = 0)
 with open('./data/pid', 'rb') as f:
   paths = pickle.load(f)
 for i in paths['PID.db']['KEGG']:
   print(i, type(paths['PID.db']['KEGG'][i]), len(paths['PID.db']['KEGG'][i]))
 kegg = paths['PID.db']['KEGG']
-#path_id = 'hsa05221'
-#process_samples(udp)
-pl = kegg['pathList']
-il = kegg['interactionList']
-mapping = kegg['mapping']
-nn = kegg['node.name']
-nt = kegg['node.type']
+path_id = 'hsa05221'
+process_samples(udp)
+#pl = kegg['pathList']
+#il = kegg['interactionList']
+#mapping = kegg['mapping']
+#nn = kegg['node.name']
+#nt = kegg['node.type']
